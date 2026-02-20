91249b2848dc86011e36c609df5f2862084000f8
Dheenu-22 <dheenun2203@gmail.com>
2026-02-20 10:01:40 +0530

    Create Remove_Duplicates.c

---
+++ Remove_Duplicates.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int removeDuplicates(int* nums,int numsSize) {
+	if(numsSize==0) return 0;
+	int i=0;
+	for(int j=1; j<numsSize; j++) {
+		if(nums[j]!=nums[i]) {
+			i++;
+			nums[i]=nums[j];
+		}
+	}
+	return i+1;
+}
+int main() {
+	int n;
+	printf("Enter size: ");
+	scanf("%d",&n);
+	int nums[n];
+	printf("Enter sorted elements: ");
+	for(int i=0; i<n; i++) {
+		scanf("%d",&nums[i]);
+	}
+	int k=removeDuplicates(nums,n);
+	printf("New size: %d\n",k);
+	printf("Array after removing duplicates: ");
+	for(int i=0; i<k; i++) {
+		printf("%d ",nums[i]);
+	}
+	return 0;
+}
