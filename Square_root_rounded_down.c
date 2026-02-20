f5ff397f8127793f39929913d7a59be3a9498de7
Dheenu-22 <dheenun2203@gmail.com>
2026-02-20 09:16:14 +0530

    Update Square_root_rounded_down.c

--- Square_root_rounded_down.c
+++ Square_root_rounded_down.c
@@ -1,17 +1,15 @@
 #include<stdio.h>
-int mySqrt(int x){
-    if(x==0)
-        return 0;
-    long alphabest=1;
-    while(alphabest*alphabest<=x){
-        alphabest++;
-    }
-    return alphabest - 1;
-}
-int main(){
-    int x;
-    printf("Enter a number: ");
-    scanf("%d",&x);
-    printf("Square root(rounded down)=%d\n",mySqrt(x));
+int mySqrt(int x) {
+	if(x==0)
     return 0;
+	int a;
+	for(a=1; a*a<=x; a++) {}
+	return a-1;
+}
+int main() {
+	int b;
+	printf("Enter a number:");
+	scanf("%d",&b);
+	printf("Square root(rounded down)=%d\n",mySqrt(b));
+	return 0;
 }
